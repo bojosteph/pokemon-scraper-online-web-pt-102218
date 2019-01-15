a9587acac6d34a3e4ad77052925ee8d1980b7413
Roberto Samia <bojosteph@gmail.com>
2019-01-15 03:22:09 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,33 @@
+
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+  
+  @@all = []
+  
+  def initialize(id, name=nil, type=nil, db=nil)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.save(name, type, database_connection)
+    database_connection.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+  end
+  
+  
+  def self.find
+
+    
+  
+  
+  
+  
 end
+
+
